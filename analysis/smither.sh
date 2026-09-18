10b5e83a24bd150c072a2201a4788d63ccc802d3
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2026-09-18 14:33:52 +0200

    Update smither.sh

--- analysis/smither.sh
+++ analysis/smither.sh
@@ -3,20 +3,17 @@
 fro=$1
 to=$2
 
-ml R_packages/4.3.1
+module load R-bundle-Bioconductor/3.20-foss-2024a-R-4.4.2
 
 outdir=$3
 inputdata=$4
-
+threshold_age=11
 
 for i in $(seq $fro $to);
 do
 
-#seed outdir inputdata
-
-Rscript fancier_varying_seed_clock.R $i $outdir $inputdata
-
-wait
+#seed outdir inputdata threshold_age
+Rscript fancier_varying_seed_clock5_F.R $i $outdir $inputdata $threshold_age
 
 
 done
