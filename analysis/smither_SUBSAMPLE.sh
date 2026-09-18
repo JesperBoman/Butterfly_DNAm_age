ab4a3d90ea5bb4cf7f793af37670fc2902e2efa4
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2026-09-18 14:37:05 +0200

    Update smither_SUBSAMPLE.sh

--- analysis/smither_SUBSAMPLE.sh
+++ analysis/smither_SUBSAMPLE.sh
@@ -8,15 +8,18 @@ module load R-bundle-Bioconductor/3.20-foss-2024a-R-4.4.2
 outdir=$3
 inputdata=$4
 samplesize=$5
+threshold_age=11
 
 for i in $(seq $fro $to);
 do
 
-#seed outdir inputdata
+#seed outdir inputdata samplesize threshold_age
 
-Rscript fancier_varying_seed_clock_SUBSAMPLE.R $i $outdir $inputdata $samplesize
+#When subsampling the E.clock
+Rscript fancier_varying_seed_clock5_F_SUBSAMPLE.R $i $outdir $inputdata $samplesize $threshold_age
 
-wait
+#When subsampling the W.clock
+Rscript fancier_varying_seed_clock5_F_WCLOCK_SUBSAMPLE.R $i $outdir $inputdata $samplesize $threshold_age
 
 
 done
